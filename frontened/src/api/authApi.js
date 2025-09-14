e33ab2bc0f7017f90f9ecfd8a9d9d935a497a1f3
Aman PS <123456+amanps1@users.noreply.github.com>
2025-09-14 11:45:42 +0530

    new changes

--- frontened/src/api/authApi.js
+++ frontened/src/api/authApi.js
@@ -1,24 +1,20 @@
-// src/api/authApi.js
+
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/auth";
 
-// Login
 export const loginUser = async (loginDto) => {
   return await axios.post(`${API_URL}/login`, loginDto);
 };
 
-// Register
 export const registerUser = async (registrationDto) => {
   return await axios.post(`${API_URL}/register`, registrationDto);
 };
 
-// Forgot Password
 export const forgotPassword = async (email) => {
   return await axios.post(`${API_URL}/forgot-password`, { email });
 };
 
-// Reset Password
 export const resetPassword = async (token, newPassword) => {
   return await axios.post(`${API_URL}/reset-password`, { token, newPassword });
 };
